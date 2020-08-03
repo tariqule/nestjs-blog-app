a476c064d9a6f1a4abb203dfb1706788bdaa3cb2
Tariq Khan <45430120+Thk101@users.noreply.github.com>
2020-08-03 19:54:28 -0400

    added models and apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+import { TagModule } from './tag/tag.module';
+import { ProfileModule } from './profile/profile.module';
+import { ArticleModule } from './article/article.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as ormconfig from '../ormconfig';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    ArticleModule,
+    UserModule,
+    ProfileModule,
+    TagModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+
+export class ApplicationModule {
+  constructor(private readonly connection: Connection) { }
+}
